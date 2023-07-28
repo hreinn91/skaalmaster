c456efb4532a21463381bef6edc0e6399ca4e96a
hreinn91 <hreinnjuliusson@gmail.com>
2023-07-28 12:15:07 +0200

    Change launch icon to Fredrisk stupid face

--- core/src/com/hreinn/skaalmaster/SkaalMaster.java
+++ core/src/com/hreinn/skaalmaster/SkaalMaster.java
@@ -12,14 +12,15 @@ public class SkaalMaster extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("fredrik01.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(25f / 255, 0, 51f / 255, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+
 		batch.end();
 	}
 	
