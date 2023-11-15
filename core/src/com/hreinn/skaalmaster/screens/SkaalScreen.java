50b559df11fae8df5a8f3c9866f3f02bb56045f8
hreinn91 <hreinnjuliusson@gmail.com>
2023-11-15 22:11:17 +0100

    Use ExtendViewPort which keeps aspect ratio. See: https://libgdx.com/wiki/graphics/viewports

--- core/src/com/hreinn/skaalmaster/screens/SkaalScreen.java
+++ core/src/com/hreinn/skaalmaster/screens/SkaalScreen.java
@@ -2,12 +2,14 @@ package com.hreinn.skaalmaster.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.hreinn.skaalmaster.SkaalMaster;
 
 public abstract class SkaalScreen implements Screen {
@@ -15,14 +17,14 @@ public abstract class SkaalScreen implements Screen {
     public static final Color BACKGROUND_COLOR = new Color((25f / 255), 0, 51f / 255, 1);
 
     private final SkaalMaster game;
-    private final OrthographicCamera camera;
-    private final ScreenViewport screenViewport;
+    private final Camera camera;
+    private final Viewport screenViewport;
     private final Stage stage;
 
     protected SkaalScreen(SkaalMaster game) {
         this.game = game;
-        camera = new OrthographicCamera(800, 480);
-        screenViewport = new ScreenViewport(camera);
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        screenViewport = new ExtendViewport(1920, 1080, camera);
         stage = new Stage(screenViewport);
         Gdx.input.setInputProcessor(stage);
     }
@@ -35,7 +37,7 @@ public abstract class SkaalScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(BACKGROUND_COLOR);
+        ScreenUtils.clear(getBackgroundColor());
         camera.update();
         game.getBatch().setProjectionMatrix(camera.combined);
 
@@ -68,6 +70,10 @@ public abstract class SkaalScreen implements Screen {
         stage.dispose();
     }
 
+    protected Color getBackgroundColor(){
+        return BACKGROUND_COLOR;
+    }
+
     protected SkaalMaster getGame() {
         return game;
     }
@@ -80,7 +86,7 @@ public abstract class SkaalScreen implements Screen {
         getStage().addActor(actor);
     }
 
-    protected OrthographicCamera getCamera() {
+    protected Camera getCamera() {
         return camera;
     }
 }
